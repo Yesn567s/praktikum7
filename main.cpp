6bbe51d192155a115ac8a85b7b7ea9b31261c851
Yesn't <reynardlee0510@gmail.com>
2024-11-30 19:03:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,27 @@ using namespace std;
 //global variables
 int gold=0, treasure=0, input_menu;
 
+//shovel durability
+int wood_shov_dur=3, stone_shov_dur=10, iron_shov_dur=25, diamond_shov_dur=50;
+
+ //shovel
+//bool wood_shovel=true, stone_shovel=false, iron_shovel=false, diamond_shovel=false;
+bool shovel[4]={true, false, false, false};
+
+string shovels(){
+    string result = "";
+
+    for (int i = 0; i < 4; i++) {
+        if (shovel[i]) {
+            result += "Owned";
+        } else {
+            result += "Not Owned";
+        }
+    }
+
+    return result;
+}
+
 //map1
 void map1(){
     char pattern[10][10] = {
@@ -88,10 +109,31 @@ void main_menu(){
     cout << ">> "; cin >> input_menu;
 }
 
+void shop_menu(){
+    cout << "== Shop ==\n";
+    cout << "Gold:\n";
+    cout << "1. Wooden shovel (Price: 0 Gold | Durability: " << wood_shov_dur << " | " << shovels() << " )" << endl;
+    cout << "2. Stone shovel (Price: 0 Gold | Durability: " << stone_shov_dur << " | " << shovels() << " )" << endl;
+    cout << "3. Iron shovel (Price: 0 Gold | Durability: " << iron_shov_dur << " | " << shovels() << " )" << endl;
+    cout << "4. Diamond shovel (Price: 0 Gold | Durability: " << diamond_shov_dur << " | " << shovels() << " )" << endl;
+}
+
 int main()
 {
+    bool cheat1, cheat2;
+
     do{
     main_menu();
+
+
+        if(input_menu==99){
+        cheat1=true;
+        gold+=1000;
+        system("cls");
+        continue;
+    }
+
+
 
     while (input_menu<0||input_menu>2){
     system("cls");
@@ -101,9 +143,9 @@ int main()
     //case 1: game  case 2: shop
     switch (input_menu){
     case 1:
-        cout << "Hello World!";
+        map1();
     case 2:
-
+        shop_menu();
     }
 
     } while (input_menu!=0);
