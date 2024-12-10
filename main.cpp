598b6227c67abd593e333aa6f9db1395c91dfe9d
Bontrex <bennyhoh2006@gmail.com>
2024-12-10 11:01:02 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,154 +1,242 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <time.h>
-#include <ctime>
+#include <math.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <iomanip>
 
 using namespace std;
 
-//global variables
-int gold=0, treasure=0, input_menu;
-
-//shovel durability
-int wood_shov_dur=3, stone_shov_dur=10, iron_shov_dur=25, diamond_shov_dur=50;
-
- //shovel
-//bool wood_shovel=true, stone_shovel=false, iron_shovel=false, diamond_shovel=false;
-bool shovel[4]={true, false, false, false};
-
-string shovels(){
-    string result = "";
-
-    for (int i = 0; i < 4; i++) {
-        if (shovel[i]) {
-            result += "Owned";
-        } else {
-            result += "Not Owned";
-        }
-    }
-
-    return result;
-}
-
-//map1
-void map1(){
-    char pattern[10][10] = {
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-        {'#', 'P', '#', 'X', ' ', 'X', '#', 'X', 'X', '#'},
-        {'#', ' ', '#', 'X', ' ', ' ', '#', ' ', ' ', '#'},
-        {'#', ' ', '#', '#', ' ', ' ', '#', 'X', ' ', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', '#', '#', '#', ' ', '#', '#', '#', ' ', '#'},
-        {'#', 'X', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'},
-        {'#', 'X', ' ', ' ', ' ', '#', ' ', '#', '#', '#'},
-        {'#', 'X', ' ', ' ', ' ', '#', ' ', ' ', 'S', '#'},
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-    };
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cout << pattern[i][j];
-        }
-        cout << endl;
-    }
-}
-
-//map2
-void map2(){
-    char pattern[10][10] = {
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-        {'#', 'P', ' ', ' ', 'X', '#', 'X', ' ', 'X', '#'},
-        {'#', ' ', ' ', ' ', 'X', '#', 'X', ' ', 'X', '#'},
-        {'#', '#', '#', ' ', 'X', 'X', 'X', ' ', '#', '#'},
-        {'#', 'X', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', ' ', '#', ' ', '#', ' ', ' ', ' ', 'X', '#'},
-        {'#', ' ', ' ', ' ', '#', ' ', ' ', 'X', '#', '#'},
-        {'#', 'X', 'X', ' ', '#', ' ', '#', '#', '#', '#'},
-        {'#', '#', 'X', ' ', '#', ' ', ' ', ' ', 'S', '#'},
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-    };
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cout << pattern[i][j];
-        }
-        cout << endl;
-    }
-}
-
-//map3
-void map3(){
-    char pattern[10][10] = {
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
-        {'#', 'P', ' ', 'X', 'X', 'X', '#', ' ', 'X', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', '#', ' ', 'X', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X', '#'},
-        {'#', ' ', ' ', ' ', 'X', 'X', ' ', ' ', ' ', '#'},
-        {'#', ' ', ' ', ' ', 'X', 'X', ' ', ' ', ' ', '#'},
-        {'#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
-        {'#', 'X', '#', ' ', ' ', ' ', ' ', 'X', 'X', '#'},
-        {'#', ' ', ' ', ' ', ' ', ' ', ' ', 'X', '#', '#'},
-        {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
-    };
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cout << pattern[i][j];
-        }
-        cout << endl;
-    }
-}
-
-//main menu
-void main_menu(){
-    cout << "=== Treasure Hunt === \n";
-    cout << "Treasure: " << treasure;
-    cout << endl << "Gold: " << gold;
-    cout << endl << "1. Play" << endl;
-    cout << "2. Shop" << endl;
-    cout << "0. Exit" << endl;
-    cout << ">> "; cin >> input_menu;
-}
-
-void shop_menu(){
-    cout << "== Shop ==\n";
-    cout << "Gold:\n";
-    cout << "1. Wooden shovel (Price: 0 Gold | Durability: " << wood_shov_dur << " | " << shovels() << " )" << endl;
-    cout << "2. Stone shovel (Price: 0 Gold | Durability: " << stone_shov_dur << " | " << shovels() << " )" << endl;
-    cout << "3. Iron shovel (Price: 0 Gold | Durability: " << iron_shov_dur << " | " << shovels() << " )" << endl;
-    cout << "4. Diamond shovel (Price: 0 Gold | Durability: " << diamond_shov_dur << " | " << shovels() << " )" << endl;
-}
-
 int main()
 {
-    bool cheat1, cheat2;
-
-    do{
-    main_menu();
-
-
-        if(input_menu==99){
-        cheat1=true;
-        gold+=1000;
-        system("cls");
-        continue;
+    srand(time(0));
+    int totalpc=0;
+    int totalcc=0;
+    string hs;
+    string chs;
+    vector <int> pc;
+    vector <int> cc;
+    vector <int> scorepc;
+    vector <int> scorecc;
+    string again;
+    while(true){
+        pc.clear();
+        cc.clear();
+        scorepc.clear();
+        scorecc.clear();
+        totalcc=0;
+        totalpc=0;
+        hs="";
+        for(int i=0;i<2;i++){
+            pc.push_back(rand()%13+1);
+        }
+        for(int i=0;i<2;i++){
+            cc.push_back(rand()%13+1);
+        }
+        for(int i=0;i<pc.size();i++){
+            scorepc.push_back(pc[i]);
+            if(scorepc[i]==1 && totalpc<21){
+                scorepc[i]=11;
+            }
+            else if(scorepc[i]==1 && totalpc>21){
+                scorepc[i]=1;
+            }
+            else if(scorepc[i]>10){
+                scorepc[i]=10;
+            }
+            totalpc+=scorepc[i];
+        }
+        cout<<"------------------------------------------"<<endl;
+        cout<<"                 Black jack"<<endl;
+        cout<<"------------------------------------------"<<endl;
+        cout<<"Your cards: ";
+        for(int i=0;i<pc.size();i++){
+            cout<<"[";
+            if(pc[i]==1){
+                cout<<"Ace]";
+            }
+            else if(pc[i]>1 && pc[i]<11){
+                cout<<pc[i]<<"]";
+            }
+            else if(pc[i]==11){
+                cout<<"Jack]";
+            }
+            else if(pc[i]==12){
+                cout<<"Queen]";
+            }
+            else if(pc[i]==13){
+                cout<<"King]";
+            }
+        }
+        cout<<endl<<"Dealer's cards: ";
+        for(int i=0;i<cc.size();i++){
+            cout<<"[";
+            if(i==0){
+                if(cc[i]==1){
+                    cout<<"Ace]";
+                }
+                else if(cc[i]>1 && cc[i]<11){
+                    cout<<cc[i]<<"]";
+                }
+                else if(cc[i]==11){
+                    cout<<"Jack]";
+                }
+                else if(cc[i]==12){
+                    cout<<"Queen]";
+                }
+                else if(cc[i]==13){
+                    cout<<"King]";
+                }
+            }
+            else{
+                cout<<"?]";
+            }
+        }
+        cout<<endl;
+        while(true){
+            cout<<"Your total points: "<<totalpc<<endl<<endl;
+            cout<<" Do you want to [h]it or [s]tand?";
+            cin>>hs;
+            if(hs=="h"){
+                pc.push_back(rand()%13+1);
+            }
+            else if(hs=="s"){
+                break;
+            }
+            hs="";
+            scorepc.clear();
+            totalpc=0;
+            cout<<"Your cards: ";
+            for(int i=0;i<pc.size();i++){
+                cout<<"[";
+                if(pc[i]==1){
+                    cout<<"Ace]";
+                }
+                else if(pc[i]>1 && pc[i]<11){
+                    cout<<pc[i]<<"]";
+                }
+                else if(pc[i]==11){
+                    cout<<"Jack]";
+                }
+                else if(pc[i]==12){
+                    cout<<"Queen]";
+                }
+                else if(pc[i]==13){
+                    cout<<"King]";
+                }
+            }
+            for(int i=0;i<pc.size();i++){
+                scorepc.push_back(pc[i]);
+                if(scorepc[i]==1 && totalpc<21){
+                    scorepc[i]=11;
+                }
+                else if(scorepc[i]==1 && totalpc>21){
+                    scorepc[i]=1;
+                }
+                else if(scorepc[i]>10){
+                    scorepc[i]=10;
+                }
+                totalpc+=scorepc[i];
+            }
+            cout<<endl;
+            if(totalpc==21){
+                cout<<"Your total points: "<<totalpc<<endl;
+                cout<<"Blackjack! You win automatically!"<<endl;
+                break;
+            }
+            else if(totalpc>21){
+                for(int i=0;i<scorepc.size();i++){
+                    if(scorepc[i]==11){
+                        scorepc
+                        [i]=1;
+                    }
+                }
+                totalpc=0;
+                for(int i=0;i<scorepc.size();i++){
+                    totalpc+=scorepc[i];
+                }
+                if(totalpc>21){
+                    cout<<"Your total points: "<<totalpc<<endl;
+                    cout<<"You busted! Dealer wins."<<endl;
+                    break;
+                }
+                else if(totalpc==21){
+                    cout<<"Your total points: "<<totalpc<<endl;
+                    cout<<"Blackjack! You win automatically!"<<endl;
+                    break;
+                }
+            }
+        }
+        if(hs=="s"){
+            cout<<"Dealer's turn..."<<endl;
+            while(true){
+                for(int i=0;i<cc.size();i++){
+                    cout<<"[";
+                    if(cc[i]==1){
+                        cout<<"Ace]";
+                    }
+                    else if(cc[i]>1 && cc[i]<11){
+                        cout<<cc[i]<<"]";
+                    }
+                    else if(cc[i]==11){
+                        cout<<"Jack]";
+                    }
+                    else if(cc[i]==12){
+                        cout<<"Queen]";
+                    }
+                    else if(cc[i]==13){
+                        cout<<"King]";
+                    }
+                }
+            cout<<endl;
+                scorecc.clear();
+                totalcc=0;
+                for(int i=0;i<cc.size();i++){
+                    scorecc.push_back(cc[i]);
+                    if(scorecc[i]==1 && totalcc<21){
+                        scorecc[i]=11;
+                    }
+                    else if(scorecc[i]==1 && totalcc>21){
+                        scorecc[i]=1;
+                    }
+                    else if(scorecc[i]>10){
+                        scorecc[i]=10;
+                    }
+                    totalcc+=scorecc[i];
+                }
+                cout<<"Dealer's total points: "<<totalcc<<endl;
+                if(totalcc>=17 || totalcc>=totalpc){
+                    break;
+                }
+                else{
+                    cout<<"Dealer's chooses to hit..."<<endl<<endl;
+                    cc.push_back(rand()%13+1);
+                }
+            }
+            if(totalcc>21){
+                cout<<"Dealer lost!"<<endl;
+            }
+            else if(totalcc>totalpc){
+                cout<<"Dealer wins!"<<endl;
+            }
+            else if(totalcc<totalpc){
+                cout<<"Player wins!"<<endl;
+            }
+            else{
+                cout<<"it is a tie"<<endl;
+            }
+        }
+        cout<<"Do you want to play again? (y/n): ";
+        cin>>again;
+        if(again=="n"){
+            break;
+        }
     }
 
 
-
-    while (input_menu<0||input_menu>2){
-    system("cls");
-    main_menu();
-    }
-
-    //case 1: game  case 2: shop
-    switch (input_menu){
-    case 1:
-        map1();
-    case 2:
-        shop_menu();
-    }
-
-    } while (input_menu!=0);
 
     return 0;
 }
